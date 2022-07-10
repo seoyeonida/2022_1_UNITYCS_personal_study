3454206c4e0010d949848c51148de0852b920c5e
seoyeonida <rsy012@naver.com>
2022-07-10 22:20:52 +0900

    유니티 C# 공부

---
+++ UNITYST/UNITYST/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNITYST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕하세요, 반갑습니다.");
+        }
+    }
+}
